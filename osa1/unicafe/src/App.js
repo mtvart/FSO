f33851963b50a29dd36e2519c6ae76b9c4b69d5f
mtvart <mika.vartiala@gmail.com>
2023-06-21 13:03:56 +0300

    commit ex 1.6

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,28 +1,41 @@
-import  React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-
-const Display = props => <div>{props.value}</div>
-
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+import { useState } from 'react'
 
 const App = () => {
-  const [value, setValue] = useState(10)
-
-  const setToValue = newValue => {
-    console.log('value now', newValue)
-    setValue(newValue)
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const handleGoodClick = () => {
+    console.log('clicked the button')
+    setGood(good + 1)
   }
+  
+    const handleNeutralClick = () => {
+    console.log('clicked the button')
+    setNeutral(neutral + 1)
+  }
+  
+    const handleBadClick = () => {
+    console.log('clicked the button')
+    setBad(bad + 1)
+  }
+
 
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <h1>give feedback</h1>
+	  <button onClick={handleGoodClick}>good</button>
+	  
+	  <button onClick={handleNeutralClick}>neutral</button>
+	  
+	  <button onClick={handleBadClick}>bad</button>
+	  
+      <h2>statistics</h2>
+      <p>good {good}</p>	
+	  <p>neutral {neutral}</p>	
+	  <p>bad {bad}</p>
+
     </div>
   )
 }
